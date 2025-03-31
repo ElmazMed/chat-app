9dc65591a90cd68d2090cec71ff0234df44efd8d
ElmazMed <elmazgour.mohamed@gmail.com>
2025-03-31 00:30:44 +0000

    Completed profile page UI

--- frontend/src/app/(protected)/profile/page.tsx
+++ frontend/src/app/(protected)/profile/page.tsx
@@ -8,16 +8,18 @@ export default function Profile() {
     <ProtectRoutes>
       <NavBar />
       <div className="flex items-center justify-center p-4">
-        <div className="bg-[#0c1425] p-5 rounded-md">
+        <div className="bg-[#0c1425] w-2/5 p-5 rounded-md">
           <div className="flex flex-col items-center ">
             <h1 className="text-lg font-semibold">Profile</h1>
             <span className="text-xs">Your profile information</span>
             <div className="my-6 relative">
-              <Image
-                src={userImg}
-                alt="Profile image"
-                className="rounded-full w-30 h-30 p-3"
-              />
+              <div className="flex items-center justify-center rounded-full h-30 w-30 border-3 border-green-600">
+                <Image
+                  src={userImg}
+                  alt="Profile image"
+                  className="rounded-full w-28 h-28"
+                />
+              </div>
               <button className="cursor-pointer h-8 w-8">
                 <div className="absolute right-[.2rem] bottom-[3rem] rounded-full bg-yellow-500 h-8 w-8 flex items-center justify-center">
                   <Camera color="black" size={19} />
@@ -25,6 +27,33 @@ export default function Profile() {
               </button>
             </div>
           </div>
+          <form className="flex flex-col gap-4">
+            <div>
+              <label className="fieldset-label mb-2">Full name</label>
+
+              <input
+                type="text"
+                className="bg-[#0f172a] rounded-lg border-slate-600 border-1 p-3 w-full outline-none"
+                placeholder="Full name"
+              />
+            </div>
+            <div>
+              <label className="fieldset-label mb-2">Email</label>
+              <input
+                type="text"
+                className="bg-[#0f172a] rounded-lg border-slate-600 border-1 p-3 w-full outline-none"
+                placeholder="example@example.com"
+              />
+            </div>
+            <div>
+              <label className="fieldset-label mb-2">Password</label>
+              <input
+                type="password"
+                className="bg-[#0f172a] rounded-lg border-slate-600 border-1 p-3 w-full outline-none"
+                placeholder="Password"
+              />
+            </div>
+          </form>
         </div>
       </div>
     </ProtectRoutes>
